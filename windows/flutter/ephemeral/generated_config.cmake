350997e00243c073ba11fd92955f0f1a5c5e6bbe
carolcarol12 <132792639+carolcarol12@users.noreply.github.com>
2023-05-07 03:35:02 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter_windows_3.7.12-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\doctor_app\\doctor_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter_windows_3.7.12-stable\\flutter"
+  "PROJECT_DIR=E:\\doctor_app\\doctor_app"
+  "FLUTTER_ROOT=C:\\src\\flutter_windows_3.7.12-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\doctor_app\\doctor_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\doctor_app\\doctor_app"
+  "FLUTTER_TARGET=E:\\doctor_app\\doctor_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\doctor_app\\doctor_app\\.dart_tool\\package_config.json"
+)
