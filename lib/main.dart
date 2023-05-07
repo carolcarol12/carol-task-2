913abd47f57d31d4759a35615f3081b7b839d5ae
carolcarol12 <132792639+carolcarol12@users.noreply.github.com>
2023-05-07 03:33:46 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:doctor_app/navigation.dart';
+import 'package:flutter/material.dart';
+
+import 'Home2.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
